6114f2164bb6c61bc76dc706e7b019d5eb58bf45
Pietro Lesci <61748653+pietrolesci@users.noreply.github.com>
2021-05-16 17:24:18 +0200

    fix: remove flickering

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 )
 
 # session state
-session = session_state.get(process=False, run_id=0, posdf=None, negdf=None)
+session = session_state.get(process=False, run_id=0, posdf=None, negdf=None, uploaded_file_id=0)
 
 
 # ==== SIDEBAR ==== #
@@ -32,47 +32,34 @@ PAGES = {
     "About": about,
 }
 
-with st.sidebar.beta_container():
-    st.sidebar.header("Navigation")
-    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-
+st.sidebar.header("Navigation")
+# with st.sidebar.beta_container():
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
 
 # FILE UPLOADER
-with st.sidebar.beta_container():
-    st.markdown("")
-    st.markdown("")
-    st.header("Upload file")
-    uploaded_file = st.sidebar.file_uploader("Select file", type=[i.name for i in SupportedFiles])
+st.sidebar.markdown("")
+st.sidebar.markdown("")
+st.sidebar.header("Upload file")
+# with st.sidebar.beta_container():
+uploaded_file = st.sidebar.file_uploader("Select file", type=[i.name for i in SupportedFiles])
 
 
 # FOOTER
-with st.sidebar.beta_container():
-    st.markdown("")
-    st.markdown("")
-    st.markdown(
-        """
-        <span style="font-size: 0.75em">Built with &hearts; by [`Pietro Lesci`](https://pietrolesci.github.io/) and [`MilaNLP`](https://twitter.com/MilaNLProc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor)</span>
-        """,
-        unsafe_allow_html=True,
-    )
+# with st.sidebar.beta_container():
+st.sidebar.markdown("")
+st.sidebar.markdown("")
+st.sidebar.markdown(
+    """
+    <span style="font-size: 0.75em">Built with &hearts; by [`Pietro Lesci`](https://pietrolesci.github.io/) and [`MilaNLP`](https://twitter.com/MilaNLProc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor)</span>
+    """,
+    unsafe_allow_html=True,
+)
 
 
 # ==== MAIN ==== #
 with st.beta_container():
     st.title("Wordify")
-    st.markdown(
-        """
-        Wordify makes it easy to identify words that discriminate categories in textual data.
-
-        Let's explain Wordify with an example. Imagine you are thinking about having a glass
-        of wine :wine_glass: with your friends :man-man-girl-girl: and you have to buy a bottle.
-        You know you like `bold`, `woody` wine but are unsure which one to choose.
-        You wonder whether there are some words that describe each type of wine.
-        Since you are a researcher :female-scientist: :male-scientist:, you decide to approach
-        the problem scientifically :microscope:. That's where Wordify comes to the rescue!
-        """
-    )
 
 
 page.write(session, uploaded_file)
